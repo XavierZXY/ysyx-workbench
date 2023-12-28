6d96d99d37ea03d4c667e4c5ebae712d27f5c814
XavierZXY <zxy_xavier@163.com>
2023-12-28 23:32:03 +0800

    fix: :bug: modify the makfile file to facilitate the construction of NVboard and WAVE simulation

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -3,14 +3,16 @@
 
 static TOP_NAME dut;
 
-void nvboard_bind_all_pins(Vdual_switch* top);
+void nvboard_bind_all_pins(Vdual_switch *top);
 
-int main() {
-    nvboard_bind_all_pins(&dut);
-    nvboard_init();
+int main()
+{
+  nvboard_bind_all_pins(&dut);
+  nvboard_init();
 
-    while(1) {
-        dut.eval();
-        nvboard_update();
-    }
+  while (1)
+  {
+    dut.eval();
+    nvboard_update();
+  }
 }
