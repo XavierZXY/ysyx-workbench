351affb77c51a6fcecc2f20bac73dfbb4a937443
XavierZXY <zxy_xavier@163.com>
2023-12-28 17:26:58 +0800

    feat: :construction: use nvboard to implement a dual switch

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,16 @@
-#include <stdio.h>
+#include <nvboard.h>
+#include "Vdual_switch.h"
+
+static TOP_NAME dut;
+
+void nvboard_bind_all_pins(Vdual_switch* top);
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+
+    while(1) {
+        dut.eval();
+        nvboard_update();
+    }
 }
